eb8abb2bec02648465a2a7a9378eea34d4c264e4
UnkelTao <wangyue121519@gmail.com>
2014-02-20 19:24:33 +0800

    add the plugin tag cloud

--- plugins/category_list_tag.rb
+++ plugins/category_list_tag.rb
@@ -1,4 +1,41 @@
 ﻿module Jekyll
+  class TagCloud < Liquid::Tag
+
+    def initialize(tag_name, markup, tokens)
+      @opts = {}
+      if markup.strip =~ /\s*counter:(\w+)/i
+        @opts['counter'] = ($1 == 'true')
+        markup = markup.strip.sub(/counter:\w+/i,'')
+      end
+      super
+    end
+
+    def render(context)
+      lists = {}
+      max, min = 1, 1
+      config = context.registers[:site].config
+      category_dir = config['root'] + config['category_dir'] + '/'
+      categories = context.registers[:site].categories
+      categories.keys.sort_by{ |str| str.downcase }.each do |category|
+        count = categories[category].count
+        lists[category] = count
+        max = count if count > max
+      end
+
+      html = ''
+      lists.each do | category, counter |
+        url = category_dir + category.to_url
+        style = "font-size: #{100 + (60 * Float(counter)/max)}%"
+        html << "<a href='#{url}' style='#{style}'>#{category}"
+        if @opts['counter']
+          html << "(#{categories[category].count})"
+        end
+        html << "</a> "
+      end
+      html
+    end
+  end
+
   class CategoryListTag < Liquid::Tag
     def render(context)
       html = ""
@@ -14,4 +51,5 @@
   end
 end
 
+Liquid::Template.register_tag('tag_cloud', Jekyll::TagCloud)
 Liquid::Template.register_tag('category_list', Jekyll::CategoryListTag)
